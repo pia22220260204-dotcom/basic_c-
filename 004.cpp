0fbec6c9a63bf057d4bd69db8b8244c25c71abdb
pia22220260204-dotcom <pia222.20260204@gmail.com>
2026-02-23 21:17:34 +0900

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -12,17 +12,18 @@ string reverse_words(string str){
     return reverse;
 }
 int main(){
-    string addr1 = "seoul";
-    string addr2 = "kang-nam";
+    string addr1 = "h";
+    string addr2 = "h";
 
     string total_addr = addr1 + " " + addr2;
 
     cout<<"address = "<<total_addr<<endl;
     cout<<"length : "<<total_addr.length()<<endl;
-    cout<<"5번째 문자 : "<<total_addr[4]<<endl;
+    cout<<"2번째 문자 : "<<total_addr[2]<<endl;
 
     string reverse = reverse_words(total_addr);
-    cout<< is_reverse(reverse,total_addr);
+    string is_reversed = is_reverse(reverse,total_addr) ? "회문" : "회문이 아님";
+    cout<< is_reversed << endl;
 
     return 0;
 }
